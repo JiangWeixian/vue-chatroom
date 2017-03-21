9786a3f46b7f4186e83d289b33aacae36cc59387
JiangWeixian <saber_wei@163.com>
2017-03-21 21:10:09 +0800

    2.2 commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,20 +1,26 @@
 <template>
-  <div>
-
+  <div class="login-page-bg">
+    <login-panel></login-panel>
   </div>
 </template>
 
 <script>
-
+  import LoginPanel from "./LoginPanel";
   export default {
-      name: 'login',
-      components: {
-          LoginPanel
-      },
-      data () {
-          return {
-              greeting: 'Hello'
-          }
-      }
+    components: {LoginPanel},
+    name: 'Login',
+    data() {
+
+    }
   }
 </script>
+
+<style scoped>
+  .login-page-bg {
+    overflow: hidden;
+    height: 100%;
+    width: 100%;
+    background:url("../assets/login-bg.jpg") no-repeat fixed center;
+  }
+
+</style>
---
+++ src/components/LoginPanel.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="LoginPanel" class="login-panel">
+    <span class="login-title" v-bind:class="[ activeNameClass? 'name':'message' ]">hello</span>
+    <div id="Logininfo" class="login-info">
+      <input type="text">
+      <input type="text">
+    </div>
+    <div id="LoginSubmit" class="login-submit">
+      <a href="#" class="signup">No Account!Sign Up</a>
+      <button type="submit">Login</button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'LoginPanel',
+    data() {
+      return {
+        activeNameClass: false
+      }
+    }
+  }
+</script>
