622ff1f305d6db6777dc3a4e099fc30124265b1d
Check Lee <453034701@qq.com>
2017-05-15 10:41:17 +0800

    Revert "Dev"

--- src/components/push.vue
+++ src/components/push.vue
@@ -1,20 +1,14 @@
 <template>
-  <div class="wrap">
-    <p>push successfully!</p>
-    <span>This is Dynamic Segment! {{ this.$route.params.id }}</span>
-  </div>
+  <p>push successfully!</p>
 </template>
 
 <script>
   export default {
     name: 'push',
     data() {
-      return ''
-    },
-    watch: {
-        '$route' (to, from) {
-            console.log('route change')
-        }
+      return {
+        push: ''
+      }
     }
   }
 </script>
